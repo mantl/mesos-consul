0c22017e0783a98ffc6d98cd06d74b1d3f7822b5
Chris Aubuchon <Chris.Aubuchon@gmail.com>
2015-12-14 16:30:21 -0600

    Add `--whitelist` option to limit service registration

--- config/config.go
+++ config/config.go
@@ -5,22 +5,24 @@ import (
 )
 
 type Config struct {
-	Refresh          time.Duration
-	Zk               string
-	LogLevel         string
-	MesosIpOrder     string
-	Healthcheck      bool
-	HealthcheckIp    string
-	HealthcheckPort  string
+	Refresh         time.Duration
+	Zk              string
+	LogLevel        string
+	MesosIpOrder    string
+	Healthcheck     bool
+	HealthcheckIp   string
+	HealthcheckPort string
+	WhiteList       []string
 }
 
 func DefaultConfig() *Config {
 	return &Config{
-		Refresh:        time.Minute,
-		Zk:             "zk://127.0.0.1:2181/mesos",
-		MesosIpOrder:   "netinfo,mesos,host",
-		Healthcheck:    false,
-		HealthcheckIp:  "127.0.0.1",
+		Refresh:         time.Minute,
+		Zk:              "zk://127.0.0.1:2181/mesos",
+		MesosIpOrder:    "netinfo,mesos,host",
+		Healthcheck:     false,
+		HealthcheckIp:   "127.0.0.1",
 		HealthcheckPort: "24476",
+		WhiteList:       []string{},
 	}
 }
