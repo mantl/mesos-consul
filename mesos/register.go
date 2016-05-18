c591c17d8564be2dfbc963dd4f3b4950ae82f77e
Dennis <charlesdarw@gmx.de>
2016-05-18 15:35:21 +0200

    Log info to debug

--- mesos/register.go
+++ mesos/register.go
@@ -103,10 +103,10 @@ func (m *Mesos) registerTask(t *state.Task, agent string) {
 	var tags []string
 
 	tname := cleanName(t.Name, m.Separator)
-        log.Infof("original TaskName : (%v)", tname)
+        log.Debugf("original TaskName : (%v)", tname)
 	if t.Label("overrideTaskName") != "" {
 		tname = cleanName(t.Label("overrideTaskName"), m.Separator)
-		log.Infof("overrideTaskName to : (%v)", tname)
+		log.Debugf("overrideTaskName to : (%v)", tname)
 	}
 	if !m.TaskPrivilege.Allowed(tname) {
 		// Task not allowed to be registered
